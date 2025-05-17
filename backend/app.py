c987854a68a253322ad90ca76f28436ecb452ed1
Chames Dinuka <101823341+chamesh2019@users.noreply.github.com>
2025-05-17 18:49:48 +0530

    nginx test

--- backend/app.py
+++ backend/app.py
@@ -3,4 +3,4 @@ from functions.views import create_app # Changed back to absolute import
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True) # Nginx will handle SSL and external port
